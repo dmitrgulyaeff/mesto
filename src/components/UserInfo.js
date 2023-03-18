4cc11e457d34acdc602591e0e878dd20c1a96594
DmitriiGuliaev <Dimajkee2000@gmail.com>
2023-03-18 09:58:59 +0700

    refactor: add methods for profile avatar

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,8 +1,9 @@
 export default class UserInfo {
   constructor(options) {
-    const { nameSelector, descriptionSelector } = options;
+    const { nameSelector, descriptionSelector, avatarSelector } = options;
     this._nameElement = document.querySelector(nameSelector);
     this._descriptionElement = document.querySelector(descriptionSelector);
+    this._avatarElement = document.querySelector(avatarSelector);
   }
 
   getUserInfo() {
@@ -12,8 +13,26 @@ export default class UserInfo {
     };
   }
 
-  setUserInfo({ newName, newDescription }) {
-    this._nameElement.textContent = newName;
-    this._descriptionElement.textContent = newDescription;
+  setNewAvatar(req) {
+    this._avatarElement.src = req["avatar"];
+  }
+
+  setNewNameAndAbout(req) {
+    this._nameElement.textContent = req["name"];
+    this._descriptionElement.textContent = req["about"];
+  }
+
+  setUserId(req) {
+    this._userId = req["_id"];
+  }
+
+  setUserInfo(req) {
+    this.setNewAvatar(req);
+    this.setNewNameAndAbout(req);
+    this.setUserId(req);
+  }
+
+  getUserId() {
+    return this._userId;
   }
 }
